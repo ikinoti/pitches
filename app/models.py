d1774dad084b238d1332651e4e883438b5b19890
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-20 02:06:02 +0300

    added a user model

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from . import db 
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True) 
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch',backref = 'users',lazy="dynamic")
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        reviews = Comment.query.filter_by(pitch_id=id).all()
+        return comments
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
