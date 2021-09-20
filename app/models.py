8c4830b56b75ef39ac26f529fe410f0c70fc8bd4
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-20 10:47:42 +0300

    add role models

--- app/models.py
+++ app/models.py
@@ -105,3 +105,13 @@ class Comment(db.Model):
         comments = Comment.query.filter_by(pitch_id=id).all()
 
         return comments
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic") 
+
+    def __repr__(self):
+        return f'User {self.name}' 
