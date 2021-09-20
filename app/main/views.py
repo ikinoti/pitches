0f21df18e36f34a50b09000e3086ffe4872f5409
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-20 15:22:22 +0300

    category view function

--- app/main/views.py
+++ app/main/views.py
@@ -102,3 +102,16 @@ def new_pitch():
         return redirect(url_for('main.index'))
 
     return render_template('new_pitch.html', new_pitch_form= form, category= category)
+
+@main.route('/category/<int:id>')
+def category(id):
+    '''
+    function that returns pitches based on the entered category id
+    '''
+    category = PitchCategory.query.get(id)
+
+    if category is None:
+        abort(404)
+
+    pitches_in_category = Pitches.get_pitch(id)
+    return render_template('category.html' ,category= category, pitches= pitches_in_category)
