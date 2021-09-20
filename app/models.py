e5579522ebc05d21c541ef974f4ef690209c3667
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-20 10:38:17 +0300

    add a comment model

--- app/models.py
+++ app/models.py
@@ -77,3 +77,31 @@ class Pitch(db.Model):
         category passed to it
         '''
         return Pitch.query.filter_by(category_id= cat_id)
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment= db.Column(db.String)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    username =  db.Column(db.String)
+    votes= db.Column(db.Integer)
+    
+
+    def save_comment(self):
+        '''
+        Function that saves comments
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_comments(cls):
+        Comment.all_comments.clear()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+
+        return comments
