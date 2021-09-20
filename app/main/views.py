3025861580208d0f783432d79ffb60cc54bf68b0
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-20 15:17:42 +0300

    pitch view function

--- app/main/views.py
+++ app/main/views.py
@@ -58,3 +58,15 @@ def product():
     title = 'Product Pitches'
     pitches= Pitch.get_all_pitches()
     return render_template('product.html', title = title, pitches= pitches )
+
+@main.route('/pitch/<int:pitch_id>')
+def pitch(pitch_id):
+
+    '''
+    View pitch page function that returns the pitch details page and its data
+    '''
+    found_pitch= get_pitch(pitch_id)
+    title = pitch_id
+    pitch_comments = Comment.get_comments(pitch_id)
+
+    return render_template('pitch.html',title= title ,found_pitch= found_pitch, pitch_comments= pitch_comments)
